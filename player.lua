d44f20625ba1e518eb54b8a2c7237293fe0745b7
Moe <moe@diccuric.org>
2015-01-23 23:51:06 +0100

    Player can die

--- player.lua
+++ player.lua
@@ -3,6 +3,10 @@ class "Player" {
   y = 0;
   dx = 0;
   dy = 0;
+  air = 100;
+  hunger = 100;
+  thurst = 100;
+  dead = false;
 }
 
 function Player:__init(x, y)
@@ -10,18 +14,45 @@ function Player:__init(x, y)
   self.y = y
   self.image = love.graphics.newImage("gfx/player.png")
   self.quad = love.graphics.newQuad(0, 0, 64, 128, self.image:getWidth(), self.image:getHeight())
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
 end
 
 function Player:draw()
+  if self.dead then
+    love.graphics.setColor(192, 0, 0, 255);
+    love.graphics.circle("fill", self.x + self.width / 2, self.y + self.height / 2 + 20, 50, 100); -- Draw white circle with 100 segments.
+  end
   love.graphics.draw(self.image, self.quad, self.x, self.y)
+  
+  love.graphics.setColor(0, 0, 255, 255)
+  love.graphics.print(tostring(self.air), self.x, self.y - 50)
+  love.graphics.setColor(255, 0, 0, 255)
+  love.graphics.print(tostring(self.hunger), self.x, self.y - 32)
+  love.graphics.setColor(0, 255, 255, 255)
+  love.graphics.print(tostring(self.thurst), self.x, self.y - 16)
+  love.graphics.setColor(255, 255, 255, 255)
 end
 
 function Player:update(dt)
   self.x = self.x + 15 * self.dx * dt
   self.y = self.y + 15 * self.dy * dt
+  if self.dead then
+    return
+  end
+  self.air = self.air - 5 * dt
+  self.thurst = self.thurst - 5 * dt
+  self.hunger = self.hunger - 2 * dt
+  if self.air < 0 or self.thurst < 0 or self.hunger < 0 then
+    self.dead = true
+  end
 end
 
 function Player:keypressed(key)
+  if self.dead then
+    return
+  end
+
   if key == 'w' then
     self.dy = -1
   elseif key == 's' then
@@ -35,6 +66,10 @@ function Player:keypressed(key)
 end
 
 function Player:keyreleased(key)
+  if self.dead then
+    return
+  end
+
   if key == 'w' and not love.keypressed('s') then
     self.dy = 0
   elseif key == 's' and not love.keypressed('w') then
