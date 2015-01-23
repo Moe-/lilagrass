290073dda87e975564fe20ce6398264e0514b80a
Moe <moe@diccuric.org>
2015-01-23 20:55:38 +0100

    Some more basic functions

--- main.lua
+++ main.lua
@@ -3,3 +3,31 @@ require('utils')
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
 end
+
+function love.load()
+  if arg[#arg] == "-debug" then 
+    require("mobdebug").start() 
+  end
+end
+
+function love.update(dt)
+  
+end
+
+function love.mousepressed(x, y, button)
+  
+end
+
+function love.mousereleased(x, y, button)
+  
+end
+
+function love.keypressed(key)
+  
+end
+
+function love.keyreleased(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
